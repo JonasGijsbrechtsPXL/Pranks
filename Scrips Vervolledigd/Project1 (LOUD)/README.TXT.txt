14b51d7a597a76c0fb56e3d41d0edc23a32d8ee4
Jonas Gijsbrechts <148559701+JonasGijsbrechtsPXL@users.noreply.github.com>
2023-11-21 21:41:32 +0100

    Add files via upload

--- Scrips Vervolledigd/Project1 (LOUD)/README.TXT.txt
+++ Scrips Vervolledigd/Project1 (LOUD)/README.TXT.txt
@@ -4,5 +4,5 @@ To edit the sound:
 Change the URL
 
 Open Powershell as Admin:
-Set-ExecutionPolicy RemoteSigned
-A
+"Set-ExecutionPolicy RemoteSigned"
+"A"
--- Scrips Vervolledigd/Project1 (LOUD)/Volume.ps1
+++ Scrips Vervolledigd/Project1 (LOUD)/Volume.ps1
@@ -3,12 +3,23 @@ $audioLevel = $audioLevel / 2;100..$audioLevel | % {$wshShell.SendKeys([char]175
 
 $url = "https://www.youtube.com/watch?v=BGUUjqL3Tgw&t=789s&ab_channel=nubileasmr"
 
-start-sleep -Seconds 60
+
+$AppName = "Chrome.exe"
+
 while ($true) {
-    set_AudioLevel -Volume 100
-    Start-Sleep -Seconds 1
-    while ($true) {
-        Start-Process $url
-        Start-Sleep -Seconds 10
+    $appRunning = Get-Process | Where-Object { $_.ProcessName -eq $AppName }
+
+    if ($appRunning) {
+        while ($true) {
+            set_AudioLevel -Volume 100
+            start-sleep -Seconds 1
+        } }
+    else {
+        while ($true) {
+            Start-Process $url
+        set_AudioLevel -Volume 100
+            Start-Sleep -Seconds 2
+        }
     }
+
 }
