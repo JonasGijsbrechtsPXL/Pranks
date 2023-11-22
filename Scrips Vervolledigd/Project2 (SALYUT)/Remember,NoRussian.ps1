a4e1eafe5d524bb2104fdf2fc3c0e796e6816dba
Jonas Gijsbrechts <148559701+JonasGijsbrechtsPXL@users.noreply.github.com>
2023-11-22 11:12:23 +0100

    Update Remember,NoRussian.ps1

--- Scrips Vervolledigd/Project2 (SALYUT)/Remember,NoRussian.ps1
+++ Scrips Vervolledigd/Project2 (SALYUT)/Remember,NoRussian.ps1
@@ -23,8 +23,6 @@ Add-Type @"
 $SPI_SETDESKWALLPAPER = 0x0014
 $SPIF_UPDATEINIFILE = 0x01
 
-[Wallpaper]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $imagePath, $SPIF_UPDATEINIFILE)
-
 #USSR Anthem
 $url = "https://www.youtube.com/watch?v=U06jlgpMtQs&ab_channel=rascrifice"
 
@@ -34,9 +32,10 @@ set_AudioLevel -Volume 100
 $speak.Speak('Salut my friend, You will join the comunism. in minus 10 seconds. 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 0. Welcome!')
 set_AudioLevel -Volume 100
 
+[Wallpaper]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $imagePath, $SPIF_UPDATEINIFILE)
+
 while ($true) {
     set_AudioLevel -Volume 100
     Start-Process $url
-    [Wallpaper]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $imagePath, $SPIF_UPDATEINIFILE)
     Start-Sleep -Seconds (10 * 60)
 }
